561b9673cda0c7e73b6fb17ac916c8003064c282
Marcell <maci20506@gmail.com>
2021-10-19 16:05:19 +0200

    3 másodperc után megváltozik a gomb háttérszíne

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-function katt() {
+let szin = "kek";
+function szinValtas() {
+    let gomb = document.getElementById("gomb");
+    if (szin === "kek") {
+        szin = "zold";
+        gomb.classList.add("zold");
+        gomb.classList.remove("kek");
+    } else {
+        szin = "kek";
+        gomb.classList.add("kek");
+        gomb.classList.remove("zold");
+    }
+}
 
+function katt() {
+    setTimeout(szinValtas, 3000);
 }
 
 function init() {
-    document.getElementById("gomb", katt);
+    document.getElementById("gomb").addEventListener("click", katt);
 }
 
 document.addEventListener("DOMContentLoaded", init);
